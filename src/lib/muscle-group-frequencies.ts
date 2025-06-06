4a34999fc1298c3a73c3a4513dd7057fb51e42e3
Jean Paulo Ath. <jeanpauloriopreto@gmail.com>
2025-06-06 20:42:05 +0000

    Usar os seguintes parametros: Peito: 3 dias Costas: 3 dias Pernas (Quadr

--- src/lib/muscle-group-frequencies.ts
+++ src/lib/muscle-group-frequencies.ts
@@ -5,16 +5,16 @@
 export const muscleGroupSuggestedFrequencies: Record<string, number> = {
   'Peito': 3,
   'Costas': 3,
-  'Pernas (Quadríceps)': 3,
-  'Pernas (Posteriores)': 3,
-  'Glúteos': 3,
-  'Ombros': 2,
-  'Bíceps': 2,
-  'Tríceps': 2,
-  'Lombar': 2,
-  'Trapézio': 2,
-  'Panturrilhas': 1,
-  'Abdômen': 1,
-  'Antebraço': 1,
+  'Pernas (Quadríceps)': 4,
+  'Pernas (Posteriores)': 4,
+  'Glúteos': 4, // Assumindo que Glúteos seguem a lógica de Pernas
+  'Ombros': 3,
+  'Bíceps': 3,
+  'Tríceps': 3,
+  'Lombar': 4,
+  'Trapézio': 3,
+  'Panturrilhas': 3,
+  'Abdômen': 2,
+  'Antebraço': 2,
   // 'Cardio' não tem um impacto direto na frequência de descanso muscular para esta lógica.
 };
