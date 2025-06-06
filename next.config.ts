73fec2b2268a88d66b02d801839cee720cccb3ab
Jean Paulo Ath. <jeanpauloriopreto@gmail.com>
2025-06-06 23:22:00 +0000

    Pode remover o genkit e as funcionalidade por ia completamente e tornar

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export', // Adicionado para exportação estática
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
