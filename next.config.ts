c012f20aa5202422031c7e8853884f6d8bc7e120
Jean Paulo Ath. <jeanpauloriopreto@gmail.com>
2025-06-07 02:11:37 +0000

    o github permite eu hospedar ele como uma página estática nele né?

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 
 import type {NextConfig} from 'next';
 
+const repositoryName = 'workout-forge';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  // Configure basePath e assetPrefix se você estiver implantando em um subdiretório no GitHub Pages
+  // Ex: https://<username>.github.io/<repositoryName>/
+  // Se estiver implantando em um domínio raiz (ex: <username>.github.io ou um domínio personalizado),
+  // essas linhas podem ser removidas ou comentadas.
+  basePath: `/${repositoryName}`,
+  assetPrefix: `/${repositoryName}/`,
   typescript: {
     ignoreBuildErrors: true,
   },
