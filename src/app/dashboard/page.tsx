81dfad05696a5364b18f2d2d61bd0c1a7546812b
Jean Paulo Ath. <jeanpauloriopreto@gmail.com>
2025-06-09 20:26:33 +0000

    Ao fazer o commit das alterações na main deu esse erro ao abrir a tela:

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ import { useAppContext } from '@/contexts/AppContext';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
 import type { UserSettings } from '@/lib/types';
